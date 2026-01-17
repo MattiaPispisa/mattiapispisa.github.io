89c12053a6a1617ff05ba8d588be1d3f8d57974e
Mattia Pispisa <mattia.pispisa@gmail.com>
2026-01-17 17:54:11 +0100

    update profile image

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,7 +1,11 @@
 const mail = "mattia.pispisa@gmail.com";
-const profileImage = "./profile_image.jpg";
+
+const _mailMd5Hash = "287506258d5dadb5a6fd12aa3a8e6c5e";
+const profileImage = `https://www.gravatar.com/avatar/${_mailMd5Hash}?s=400`;
+
 const resumeIt = "resume.it.pdf";
 const resumeEn = "resume.en.pdf";
+
 const igMontanri = "https://www.instagram.com/montanari_scarsi/";
 const merito = "merito.jpeg";
 const profileSite = "http://mattiapispisa.it";
